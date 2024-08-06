9b8ef75be73b7628d02a300676c790fd62bf3faf
Asad <muhammadasad98980@gmail.com>
2024-08-06 13:43:06 +0500

    Updated Email Config

---
+++ emailConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: "smtp.gmail.com",
+  port: 587, // or 465 for SSL
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "lookflock@gmail.com",
+    pass: "swhx jvsa eavu ciny",
+  },
+};
--- routes/emailService.js
+++ routes/emailService.js
@@ -1,26 +1,26 @@
 const router = require("express").Router();
-const nodemailer = require('nodemailer');
-const { emailConfig } = require('../emailConfig');
+const nodemailer = require("nodemailer");
+const emailConfig = require("../emailConfig.js");
 
 const transporter = nodemailer.createTransport(emailConfig);
 
-router.post('/', async (req, res) => {
-    const { to, subject, text, html } = req.body;
+router.post("/", async (req, res) => {
+  const { to, subject, text, html } = req.body;
 
-    const mailOptions = {
-        from: "Lookflock <muhammadasad98980@gmail.com>",
-        to,
-        subject,
-        text,
-        html
-    };
+  const mailOptions = {
+    from: "Lookflock <muhammadasad98980@gmail.com>",
+    to,
+    subject,
+    text,
+    html,
+  };
 
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        res.status(200).send({ message: 'Email sent', info });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    res.status(200).send({ message: "Email sent", info });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
 module.exports = router;
